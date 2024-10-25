a5f15ba3824a381760136c0686934203c8b9e1a3
Maurício Ventura Nardelli <mauricio.vnardelli@gmail.com>
2024-10-25 13:00:32 -0300

    Correcao da midia

--- components/media.tsx
+++ components/media.tsx
@@ -2,8 +2,6 @@
 
 import { IMediaPanel } from "@/lib/definitions";
 import { useEffect, useState } from "react";
-import { Loading } from "./ui/loading";
-import { getBlobFromUrl } from "@/lib/actions";
 
 interface IMediaProps {
   medias: IMediaPanel[];
@@ -11,31 +9,7 @@ interface IMediaProps {
 }
 
 export function Media({ medias, ...otherProps }: IMediaProps) {
-  const [currentMedia, setCurrentMedia] = useState<number>();
-  const [loadingMedia, setLoadingMedia] = useState("");
-  const [blobs, setBlobs] = useState<string[]>();
-
-  async function addMediasDB() {
-    const uploadMedias = medias.map(async (media) => {
-      setLoadingMedia(media.name);
-      const blob = await getBlobFromUrl(media.file);
-
-      return URL.createObjectURL(blob);
-    });
-
-    await Promise.all(uploadMedias).then((value) => {
-      setBlobs(value);
-      setCurrentMedia(0);
-    });
-  }
-
-  useEffect(() => {
-    addMediasDB();
-
-    return () => {
-      blobs?.forEach((url) => URL.revokeObjectURL(url));
-    };
-  }, [medias]);
+  const [currentMedia, setCurrentMedia] = useState<number>(0);
 
   useEffect(() => {
     if (currentMedia !== undefined && medias.length > 1) {
@@ -51,50 +25,25 @@ export function Media({ medias, ...otherProps }: IMediaProps) {
     }
   }, [currentMedia]);
 
-  if (currentMedia !== undefined && blobs) {
-    const url = blobs[currentMedia];
-
-    return (
-      <div id="component-media" className={otherProps.className}>
-        {medias[currentMedia].mediaType == "IMAGEM" ? (
-          <img
-            key={medias[currentMedia].id}
-            alt={medias[currentMedia].name}
-            src={url}
-            className="w-full h-full max-h-screen"
-          />
-        ) : (
-          <div>
-            <button
-              onClick={() => {
-                const video = document.getElementById(
-                  "video"
-                ) as HTMLVideoElement;
-                video.play();
-              }}
-              className="text-white"
-            >
-              PLAY
-            </button>
-            <a href={url}>{url}</a>
-            <video
-              id="video"
-              key={medias[currentMedia].id}
-              className="w-full h-full max-h-screen"
-              //autoPlay
-              loop={medias.length == 1 ? true : false}
-            >
-              <source src={url} type="video/mp4" />
-            </video>
-          </div>
-        )}
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <Loading text={'Carregando... "' + loadingMedia + '"'} />
+    <div id="component-media" className={otherProps.className}>
+      {medias[currentMedia].mediaType == "IMAGEM" ? (
+        <img
+          key={medias[currentMedia].id}
+          alt={medias[currentMedia].name}
+          src={medias[currentMedia].file}
+          className="w-full h-full max-h-screen"
+        />
+      ) : (
+        <video
+          key={medias[currentMedia].id}
+          className="w-full h-full max-h-screen"
+          autoPlay
+          loop={medias.length == 1 ? true : false}
+        >
+          <source src={medias[currentMedia].file} type="video/mp4" />
+        </video>
+      )}
     </div>
   );
 }
