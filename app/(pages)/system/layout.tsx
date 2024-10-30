4807f464cecf11837bb27d77df46fcde2a418c56
Maurício Ventura Nardelli <mauricio.vnardelli@gmail.com>
2024-10-29 21:36:14 -0300

    Remoção do serviceworker

--- app/(pages)/system/layout.tsx
+++ app/(pages)/system/layout.tsx
@@ -4,7 +4,6 @@ import clsx from "clsx";
 import NavBar from "@/components/ui/navbar";
 import { GlobalContext } from "@/app/contexts/global";
 import { useContext, useEffect, useState } from "react";
-import { toast } from "sonner";
 
 export default function SystemLayout({
   children,
@@ -17,21 +16,6 @@ export default function SystemLayout({
   useEffect(() => {
     setUserTV(user?.role == "TV");
   }, [user]);
-
-  useEffect(() => {
-    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then((registration) => {
-          console.log("Service Worker registrado com sucesso");
-        })
-        .catch((error) => {
-          console.log("Falha no registro do Service Worker:", error);
-        });
-    } else {
-      toast.error("Navegador SEM suporte ao Service Worker");
-    }
-  }, []);
 
   return (
     <section className="flex flex-col h-full min-h-screen bg-gray-900">
